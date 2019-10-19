7f354e22b91d202334cb57abbc0b3fe6b3c21240
Kayla Brewer <cooper.kaylam@gmail.com>
2019-10-18 20:35:55 -0500

    WHOO! 2400+ wig links

--- scrapingwigs/scrapingwigs/spiders/wig_links.py
+++ scrapingwigs/scrapingwigs/spiders/wig_links.py
@@ -4,7 +4,9 @@ import scrapy
 
 class WigLinkSpider(scrapy.Spider):
     name = "WigLinks"
-    start_urls = ["https://www.namebrandwigs.com/collections/all-items.html?_=pf&pf_pt_product_type=wig&pf_pt_product_type=Enhancer"]
+    start_urls = [
+        "https://www.namebrandwigs.com/collections/all-items.html?_=pf&pf_pt_product_type=wig&pf_pt_product_type=Enhancer"
+    ]
 
     custom_settings = {"FEED_URI": "wiglinks_nbw.json", "FEED_FORMAT": "json"}
 
@@ -17,12 +19,14 @@ class WigLinkSpider(scrapy.Spider):
             )
 
     def parse(self, response):
-        # https://docs.scrapy.org/en/latest/topics/shell.html#invoking-the-shell-from-spiders-to-inspect-responses
-        from scrapy.shell import inspect_response
+        # # https://docs.scrapy.org/en/latest/topics/shell.html#invoking-the-shell-from-spiders-to-inspect-responses
+        # from scrapy.shell import inspect_response
 
-        inspect_response(response, self)
+        # inspect_response(response, self)
 
-        wigs_links_on_page = response.xpath("//div[contains(@class,'bc-al-exist')]//a/@href").getall()
+        wigs_links_on_page = response.xpath(
+            "//div[contains(@class,'bc-al-exist')]//a/@href"
+        ).getall()
 
         for link in wigs_links_on_page:
 
@@ -31,4 +35,8 @@ class WigLinkSpider(scrapy.Spider):
         next_page = response.xpath("//a[contains(.,'→')]/@href").extract_first()
 
         if next_page:
-            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+            yield scrapy.Request(
+                response.urljoin(next_page),
+                self.parse,
+                meta={"splash": {"endpoint": "render.html", "args": {"wait": 1.0}}},
+            )
