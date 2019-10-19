465f5575c9b55b423915936ed975f6f589384fa6
Kayla Brewer <cooper.kaylam@gmail.com>
2019-10-18 23:51:39 -0500

    just black formatting and sorting imports

--- scrapingwigs/scrapingwigs/spiders/each_wig.py
+++ scrapingwigs/scrapingwigs/spiders/each_wig.py
@@ -1,28 +1,28 @@
 import json
-import scrapy
-import js2xml
 
-from scrapingwigs.scrapingwigs.items import Wig 
+import js2xml
+import scrapy
+
+from scrapingwigs.items import Wig
+
 
 class EachWigSpider(scrapy.Spider):
     name = "EachWig"
 
     def start_requests(self):
-        with open('wiglinks_nbw.json', 'r') as f:
+        with open("wiglinks_nbw.json", "r") as f:
             data = json.loads(f.read())
         # for item in data:
-        yield scrapy.Request(f"https://www.namebrandwigs.com{data[0]['link']}")
+        yield scrapy.Request(f"https://www.namebrandwigs.com{data[1]['link']}")
 
-
-    custom_settings= { 
-        'FEED_URI': "each_wig.json",
-        'FEED_FORMAT': 'json'
-    }
+    custom_settings = {"FEED_URI": "each_wig.json", "FEED_FORMAT": "json"}
 
     def parse(self, response):
         print(f"procesing: {response.url}")
-        
-        product_json = response.xpath('//script[@type="application/json"]//text()').getall()[-1]
+
+        product_json = response.xpath(
+            '//script[@type="application/json"]//text()'
+        ).getall()[-1]
 
         product_tree = json.loads(product_json)
 
@@ -35,11 +35,5 @@ class EachWigSpider(scrapy.Spider):
         scraped_wig["brand"] = product_tree["vendor"]
         scraped_wig["main_image"] = f"http:{product_tree['featured_image']}"
         scraped_wig["cap_size"] = product_tree["variants"][0]["option2"]
-        
+
         yield scraped_wig
-            
-            
-
-
-
-
