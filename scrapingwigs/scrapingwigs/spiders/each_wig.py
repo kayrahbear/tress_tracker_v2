9bab4d4d7cb60e3a726efac0f83346dab64a60ac
Kayla Brewer <cooper.kaylam@gmail.com>
2019-10-20 00:44:51 -0500

    adding a download delay to the spider. Once I let it loose to crawl all the wig listings, an instant download speed is going to be too fast.

--- scrapingwigs/scrapingwigs/spiders/each_wig.py
+++ scrapingwigs/scrapingwigs/spiders/each_wig.py
@@ -10,17 +10,17 @@ class EachWigSpider(scrapy.Spider):
     def start_requests(self):
         with open("wiglinks_nbw.json", "r") as f:
             data = json.loads(f.read())
-        # for item in data[1]:
-        yield scrapy.Request(f"https://www.namebrandwigs.com{data[0]['link']}")
+        for item in data[:10]:
+            yield scrapy.Request(f"https://www.namebrandwigs.com{item['link']}")
 
-    custom_settings = {"FEED_URI": "each_wig.json", "FEED_FORMAT": "json"}
+    custom_settings = {"FEED_URI": "each_wig.csv", "FEED_FORMAT": "csv"}
 
     def parse(self, response):
         print(f"procesing: {response.url}")
 
-        from scrapy.shell import inspect_response
+        # from scrapy.shell import inspect_response
 
-        inspect_response(response, self)
+        # inspect_response(response, self)
 
         product_json = response.xpath(
             '//script[@type="application/json"]//text()'
