abd95b29d9e0dbd4a9e82f561c7ca6e92a4fa62a
Kayla Brewer <cooper.kaylam@gmail.com>
2019-10-21 21:41:43 -0500

    forgot to add the pipeline to the settings

--- scrapingwigs/scrapingwigs/settings.py
+++ scrapingwigs/scrapingwigs/settings.py
@@ -73,9 +73,9 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'scrapingwigs.pipelines.ScrapingwigsPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'scrapingwigs.pipelines.ScrapingwigsPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
