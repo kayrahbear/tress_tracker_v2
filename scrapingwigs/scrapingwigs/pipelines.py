e3b9eba488d489bbb34d876e976dee1dd320f97a
Kayla Brewer <cooper.kaylam@gmail.com>
2019-10-21 21:38:43 -0500

    about to test my tiny pipeline. Well this is just the cleaning part. But I hope it works!

--- scrapingwigs/scrapingwigs/pipelines.py
+++ scrapingwigs/scrapingwigs/pipelines.py
@@ -1,3 +1,5 @@
+import re
+from scrapingwigs.items import Wig
 # -*- coding: utf-8 -*-
 
 # Define your item pipelines here
@@ -7,5 +9,26 @@
 
 
 class ScrapingwigsPipeline(object):
-    def process_item(self, item, spider):
-        return item
+    def process_item(self, Wig, spider):
+        #move the decimal in the price int
+        format_price = Wig['price']/100
+        
+        #remove the extra characters from the wig name. I wrote a regex!
+        wig_name_str =  Wig['wig_name']
+        try:
+            only_wig_name = re.search(' - (.+?) #', wig_name_str).group(1)
+        except AttributeError:
+            only_wig_name = ""
+        
+        #get those price marks out of the color name
+        color_name = Wig['color_name'].split(' +$')[0]
+
+        #ughh why do some of the brands have SALE on the end?!
+        brand_name = Wig['brand'].splt(" S")[0]
+
+        Wig['price'] = format_price
+        Wig['wig_name'] = only_wig_name
+        Wig['color_name'] = color_name
+        Wig['brand_name'] = brand_name
+
+        return Wig
